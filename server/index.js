60ef685595ed520225104c02b9975e75dca32f1c
Joey Feng <jfeng702@gmail.com>
2020-03-13 21:06:41 -0400

    u

--- server/index.js
+++ server/index.js
@@ -41,12 +41,13 @@ app.get("/", (req, res) => {
 
 app.get("/values/all", async (req, res) => {
   const values = await pgClient.query("SELECT * from values");
-
+  console.log('values', values)
   res.send(values.rows);
 });
 
 app.get("/values/current", async (req, res) => {
   redisClient.hgetall("values", (err, values) => {
+    console.log('values', values)
     res.send(values);
   });
 });
@@ -60,8 +61,9 @@ app.post("/values", async (req, res) => {
   redisClient.hset("values", index, "Nothing yet!");
   redisPublisher.publish("insert", index);
   pgClient.query("INSERT INTO values(number) VALUES ($1)", [index]);
-
-  res.send({ working: true });
+  const values = await pgClient.query("SELECT * from values");
+  console.log('values', values)
+  res.send({ working: true, values: values.rows });
 });
 
 app.listen(5000, err => {
